e46359206e5afe4374399d2f25f93cda3bde8a1c
Danish Agarwal <danishagarwal9@gmail.com>
2023-01-06 15:30:10 +0530

    Adding Key

--- Coding/App.js
+++ Coding/App.js
@@ -1,12 +1,17 @@
-//Removed an unimportant comment
-const heading2 = React.createElement("h1", {}, "Hello ReactJS");
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const heading2 = React.createElement("h1", { key: "h1" }, "Hello ReactJS");
 console.log(heading2); //This is the react element which is an object
 
-const heading3 = React.createElement("h2", {}, "Hello ReactJS2");
+const heading3 = React.createElement(
+  "h2",
+  { key: "h2" },
+  "Hello ReactJS2 testing"
+);
 // Now we have to put this heading2,3 into a div container
 
-const container = React.createElement("div", {}, [heading2,heading3]); //Creating container div
-
+const container = React.createElement("div", {}, [heading2, heading3]); //Creating container div
 
 const root2 = ReactDOM.createRoot(document.getElementById("root"));
 
