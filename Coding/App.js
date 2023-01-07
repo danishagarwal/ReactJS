34d382887fb1403ada9e5f7af9f98568ad5c9b5a
Danish Agarwal <danishagarwal9@gmail.com>
2023-01-07 18:41:27 +0530

    Adding basic JSX

--- Coding/App.js
+++ Coding/App.js
@@ -1,19 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const heading2 = React.createElement("h1", { key: "h1" }, "Hello ReactJS");
-console.log(heading2); //This is the react element which is an object
-
-const heading3 = React.createElement(
-  "h2",
-  { key: "h2" },
-  "Hello ReactJS2 testing"
+//React.createElement => Object => HTMLDOM
+const heading2 = React.createElement(
+  "h1",
+  { key: "h1" },
+  "Hello ReactJSS Bleh"
 );
-// Now we have to put this heading2,3 into a div container
 
-const container = React.createElement("div", {}, [heading2, heading3]); //Creating container div
-
+//JSX => React.createElement => Object => HTMLDOM
+const h = (
+  <h1
+    key="test"
+    id="title">
+    Dan Rocks
+  </h1>
+);
+console.log(h);
 const root2 = ReactDOM.createRoot(document.getElementById("root"));
 
 // React will override everything inside the div root {Interview question}
-root2.render(container);
+root2.render(h);
