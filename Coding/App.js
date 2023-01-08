14225a6b6a9c4066b8d01bf86314a6c584dcef3f
Danish Agarwal <danishagarwal9@gmail.com>
2023-01-08 08:37:49 +0530

    React.createElement vs JSX

--- Coding/App.js
+++ Coding/App.js
@@ -1,23 +1,35 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-//React.createElement => Object => HTMLDOM
-const heading2 = React.createElement(
-  "h1",
-  { key: "h1" },
-  "Hello ReactJSS Bleh"
-);
-
 //JSX => React.createElement => Object => HTMLDOM
-const h = (
+const Hola = () => (
   <h1
     key="test"
     id="title">
     Dan Rocks
   </h1>
 );
-console.log(h);
-const root2 = ReactDOM.createRoot(document.getElementById("root"));
 
-// React will override everything inside the div root {Interview question}
-root2.render(h);
+//Nested Header element with React.createElement
+const headingg = React.createElement("div", {}, [
+  React.createElement("h1", { key: "h1" }, "This is h11"),
+  React.createElement("h2", { key: "h2" }, "This is h22"),
+]);
+
+//Using JSX
+const Head1 = () => {
+  return (
+    <div>
+      {<Hola></Hola>} {/* Component composition */}
+      <h1
+        title="hoy"
+        id="test">
+        Hey H1
+      </h1>
+      <h2>Hey H2</h2>
+    </div>
+  );
+};
+
+const root2 = ReactDOM.createRoot(document.getElementById("root"));
+root2.render(Head1());
