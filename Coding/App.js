1993c23c326be9aa12eddfb1b555ebd592d7ea6a
Danish Agarwal <danishagarwal9@gmail.com>
2023-01-10 15:28:11 +0530

    Initial commit

--- Coding/App.js
+++ Coding/App.js
@@ -1,35 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-//JSX => React.createElement => Object => HTMLDOM
-const Hola = () => (
-  <h1
-    key="test"
-    id="title">
-    Dan Rocks
-  </h1>
-);
-
-//Nested Header element with React.createElement
-const headingg = React.createElement("div", {}, [
-  React.createElement("h1", { key: "h1" }, "This is h11"),
-  React.createElement("h2", { key: "h2" }, "This is h22"),
-]);
-
-//Using JSX
-const Head1 = () => {
-  return (
-    <div>
-      {<Hola></Hola>} {/* Component composition */}
-      <h1
-        title="hoy"
-        id="test">
-        Hey H1
-      </h1>
-      <h2>Hey H2</h2>
-    </div>
-  );
-};
+const AppLayout = () => {};
 
 const root2 = ReactDOM.createRoot(document.getElementById("root"));
-root2.render(Head1());
+root2.render(Header());
