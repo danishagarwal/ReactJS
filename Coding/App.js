51498d761ad939a1a05618915fa5e5b0ad2e19c9
Danish Agarwal <danishagarwal9@gmail.com>
2023-01-11 10:48:41 +0530

    Adding Nav, Header

--- Coding/App.js
+++ Coding/App.js
@@ -1,7 +1,37 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const AppLayout = () => {};
+const Title = () => {
+  return (
+    <React.Fragment>
+      <img className="image" alt="image" src="https://upload.wikimedia.org/wikipedia/commons/1/15/Food_Town_Webster.jpg"></img>
+
+      <h1 id="Title" key="h1">
+        Dan's Food Town
+      </h1>
+    </React.Fragment>
+  );
+};
+
+const HeaderComponent = () => {
+  return (
+    <div className="header">
+      <Title />
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Contact</li>
+          <li>Cart</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+const BodyComponent = () => { };
+
+const AppLayout = () => { };
 
 const root2 = ReactDOM.createRoot(document.getElementById("root"));
-root2.render(Header());
+root2.render(<HeaderComponent />);
