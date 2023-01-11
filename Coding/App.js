450119dc89add105227a2e19da88574f3c5daa80
Danish Agarwal <danishagarwal9@gmail.com>
2023-01-11 11:06:39 +0530

    Inline styling React

--- Coding/App.js
+++ Coding/App.js
@@ -4,14 +4,19 @@ import ReactDOM from "react-dom/client";
 const Title = () => {
   return (
     <React.Fragment>
-      <img className="image" alt="image" src="https://upload.wikimedia.org/wikipedia/commons/1/15/Food_Town_Webster.jpg"></img>
-
-      <h1 id="Title" key="h1">
+      <img
+        className="image"
+        alt="image"
+        src="https://upload.wikimedia.org/wikipedia/commons/1/15/Food_Town_Webster.jpg"
+      ></img>
+      <h1 style={styleobj} id="Title" key="h1">
         Dan's Food Town
       </h1>
     </React.Fragment>
   );
 };
+
+const styleobj = { backgroundColor: "red" };
 
 const HeaderComponent = () => {
   return (
@@ -29,9 +34,16 @@ const HeaderComponent = () => {
   );
 };
 
-const BodyComponent = () => { };
+const Body = () => {};
 
-const AppLayout = () => { };
+const Footer = () => {};
+const AppLayout = () => {
+  <>
+    <HeaderComponent />
+    <Body />
+    <Footer />
+  </>;
+};
 
 const root2 = ReactDOM.createRoot(document.getElementById("root"));
 root2.render(<HeaderComponent />);
