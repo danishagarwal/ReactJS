cf053ad8b93eafab964131c7d18f5bade263b1ca
Danish Agarwal <danishagarwal9@gmail.com>
2023-01-11 14:50:34 +0530

    Adding single static Restaurant Card

--- Coding/App.js
+++ Coding/App.js
@@ -7,10 +7,9 @@ const Title = () => {
       <img
         className="image"
         alt="image"
-        src="https://upload.wikimedia.org/wikipedia/commons/1/15/Food_Town_Webster.jpg"
-      ></img>
+        src="https://upload.wikimedia.org/wikipedia/commons/1/15/Food_Town_Webster.jpg"></img>
       <h1 id="Title" key="h1">
-        Dan's Food Town Cafe
+        Dan's Food Town
       </h1>
     </React.Fragment>
   );
@@ -18,33 +17,49 @@ const Title = () => {
 
 const HeaderComponent = () => {
   return (
-    <div className="header">
-      <Title />
-      <div className="nav-items">
-        <ul>
-          <li>Home</li>
-          <li>About</li>
-          <li>Contact</li>
-          <li>Cart</li>
-          <li>Meh</li>
-        </ul>
+    <>
+      <div className="header">
+        <Title />
+        <div className="nav-items">
+          <ul>
+            <li>Home</li>
+            <li>About</li>
+            <li>Contact</li>
+            <li>Cart</li>
+          </ul>
+        </div>
       </div>
+    </>
+  );
+};
+
+const RestaurantCard = () => {
+  return (
+    <div className="card">
+      <img
+        src="https://b.zmtcdn.com/data/pictures/chains/1/49781/ee586b2cda6facecb181cb3850d474ed_featured_v2.jpg"
+        alt="burgerKing"
+      />
+      <h2>Burger King</h2>
+      <h3>Burgers, American</h3>
+      <h4>4.2 stars</h4>
     </div>
   );
 };
 
 const Body = () => {
-  <h1>Hello</h1>;
+  return <RestaurantCard></RestaurantCard>;
 };
 
 const Footer = () => {};
 const AppLayout = () => {
-  <>
-    <HeaderComponent />
-    <Body />
-    <Footer />
-  </>;
+  return (
+    <>
+      <HeaderComponent />
+      <Body></Body>
+    </>
+  );
 };
 
 const root2 = ReactDOM.createRoot(document.getElementById("root"));
-root2.render(<HeaderComponent />);
+root2.render(<AppLayout />);
