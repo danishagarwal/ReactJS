d8fab183429d2dde571db2a2982410bad6678035
Danish Agarwal <danishagarwal9@gmail.com>
2023-01-15 19:12:55 +0530

    VirutalDOM example

--- Coding/components/Body.js
+++ Coding/components/Body.js
@@ -7,6 +7,7 @@ const Body = () => {
   //useState() is a hook which returns an array
   const [searchText, setsearchText] = useState("");
 
+  const [searchClicked, setsearchClicked] = useState("False");
   return (
     <>
       <div className="search">
@@ -18,7 +19,23 @@ const Body = () => {
           // onChange this function setsearchText will be called, and it's a callback function which takes 'e'
           onChange={(e) => setsearchText(e.target.value)}
         />
-        <button className="search-btn">Search</button>
+
+        {/* Toggle button which makes true -> False -> True */}
+        {/* React is very fast it will only re-render the thing which is changing because Virtual DOM /Diff Algorithm */}
+
+        <h1>{searchClicked}</h1>
+        <button
+          onClick={() => {
+            if (searchClicked == "False") {
+              setsearchClicked("True");
+            } else {
+              setsearchClicked("False");
+            }
+          }}
+          className="search-btn"
+        >
+          Search
+        </button>
         {/* {console.log(searchText)} */}
       </div>
 
