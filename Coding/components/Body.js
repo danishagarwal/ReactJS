fe70162e06bb5fc799ac6b448f31ae8a120b696e
Danish Agarwal <danishagarwal9@gmail.com>
2023-01-15 13:58:32 +0530

    SearchBar, useState

--- Coding/components/Body.js
+++ Coding/components/Body.js
@@ -1,16 +1,38 @@
 //This will import all the constants from the tab.
 import * as XYZ from "../constants";
 import RestaurantCard from "./RestaurantCard";
+import { useState } from "react";
 
 const Body = () => {
+  //useState() is a hook which returns an array
+  const [searchText, setsearchText] = useState("");
+
   return (
-    <div className="restaurantList">
-      {XYZ.restaurantList.map((restaurantObj) => {
-        return (
-          <RestaurantCard {...restaurantObj.data} key={restaurantObj.data.id} />
-        );
-      })}
-    </div>
+    <>
+      <div className="search">
+        <input
+          className="search-input"
+          placeholder="Search"
+          type="text"
+          value={searchText}
+          // onChange this function setsearchText will be called, and it's a callback function which takes 'e'
+          onChange={(e) => setsearchText(e.target.value)}
+        />
+        <button className="search-btn">Search</button>
+        {/* {console.log(searchText)} */}
+      </div>
+
+      <div className="restaurantList">
+        {XYZ.restaurantList.map((restaurantObj) => {
+          return (
+            <RestaurantCard
+              {...restaurantObj.data}
+              key={restaurantObj.data.id}
+            />
+          );
+        })}
+      </div>
+    </>
   );
 };
 
