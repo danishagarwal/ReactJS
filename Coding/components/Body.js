bb577238ca364a3f7dc074e151528886f4a50900
Danish Agarwal <danishagarwal9@gmail.com>
2023-01-18 10:43:20 +0530

    getting live data, useEffect

--- Coding/components/Body.js
+++ Coding/components/Body.js
@@ -1,8 +1,7 @@
 //This will import all the constants from the tab.
-//Comments
 import * as XYZ from "../constants";
 import RestaurantCard from "./RestaurantCard";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 function filterData(searchText, restaurants) {
   return restaurants.filter((restaurant) =>
@@ -17,6 +16,22 @@ const Body = () => {
   //This is for maitaining the restaurantList
   const [restaurants, setrestaurants] = useState(XYZ.restaurantList);
 
+  useEffect(() => {
+    getRestaurants();
+  }, []);
+
+  async function getRestaurants() {
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9351929&lng=77.62448069999999&page_type=DESKTOP_WEB_LISTING"
+    );
+
+    //Converting data into json readable format
+    const json = await data.json();
+    console.log(json);
+    setrestaurants(json?.data?.cards[2]?.data?.data?.cards);
+  }
+
+  console.log(":render");
   return (
     <>
       <div className="search">
@@ -25,24 +40,20 @@ const Body = () => {
           placeholder="Search"
           type="text"
           value={searchText}
-          // onChange this function setsearchText will be called, and it's a callback function which takes 'e'
           onChange={(e) => setsearchText(e.target.value)}
         />
+        {/* onChange this function setsearchText will be called, and it's a callback function which takes 'e' */}
 
         <button
           onClick={() => {
-            //Need to filter data
-            //Search text will be the searchbar and from restaurants we have to search
-            const data = filterData(searchText, restaurants);
+            const data = filterData(searchText, restaurants); //Need to filter data - //Search text will be the searchbar and from restaurants we have to search
 
-            //Update the restaurants with the filter data
-            setrestaurants(data);
+            setrestaurants(data); //Update the restaurants with the filter data
           }}
           className="search-btn"
         >
           Search
         </button>
-        {/* {console.log(searchText)} */}
       </div>
 
       <div className="restaurantList">
