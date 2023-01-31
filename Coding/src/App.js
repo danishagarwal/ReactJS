e7f24da585f90c78eda954c8bf302308e5fd0a89
Danish Agarwal <danishagarwal9@gmail.com>
2023-01-31 12:08:54 +0530

    classy

--- Coding/src/App.js
+++ Coding/src/App.js
@@ -8,6 +8,8 @@ import About from "./components/About";
 import Error from "./components/Error";
 import Contact from "./components/Contact";
 import RestaurantMenu from "./components/RestaurantMenu";
+import Profile from "./components/Profile";
+import ProfileClass from "./components/ProfileClass";
 
 const AppLayout = () => {
   return (
@@ -32,6 +34,12 @@ const appRouter = createBrowserRouter([
       {
         path: "/about",
         element: <About />,
+        children: [
+          {
+            path: "profile",
+            element: <ProfileClass />, //Render this in About since render children from OUTLET
+          },
+        ],
       },
       {
         path: "/contact",
--- Coding/src/components/About.js
+++ Coding/src/components/About.js
@@ -1,9 +1,28 @@
-const About = () => {
-  return (
-    <div className="About">
-      <h1>Hi, Dan here!</h1>
-    </div>
-  );
-};
+import React from "react";
+import { Outlet } from "react-router-dom";
+import Profile from "./Profile";
+import ProfileClass from "./ProfileClass";
+
+class About extends React.Component {
+  constructor(props) {
+    super(props);
+
+    console.log("Parent - Constructor");
+  }
+
+  componentDidMount() {
+    console.log("Parent- Did Mount");
+  }
+
+  render() {
+    console.log("Parent- Render");
+    return (
+      <div className="About">
+        <h1>Hi, Dan here!</h1>
+        <ProfileClass name={"First Name"} />
+      </div>
+    );
+  }
+}
 
 export default About;
